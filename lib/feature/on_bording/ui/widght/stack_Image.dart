ced9b03ce6846485d6f76430ff7df033e01e9144
mohammedahmeddd31234 <mohammedahmeddd31234@gmail.com>
2025-05-17 14:41:32 +0300

    -lohin page ui . -api login .

---
+++ lib/feature/on_bording/ui/widght/stack_Image.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../core/theme/colors.dart';
+
+class Stack_Image extends StatelessWidget {
+  const Stack_Image({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          width: 443.w,
+          height: 443.h,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+            image: AssetImage("assets/Image.png"),
+            fit: BoxFit.cover,
+          )),
+        ),
+        Container(
+          width: 375.w,
+          height: 491.h,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+            image: AssetImage("assets/Group.png"),
+            fit: BoxFit.cover,
+          )),
+        ),
+        Positioned(
+          top: 240.h,
+          child: Column(
+            children: [
+              Container(
+                alignment: Alignment.bottomCenter,
+                width: 375.w,
+                height: 239.h,
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.bottomCenter,
+                        end: Alignment.topCenter,
+                        colors: [
+                      Colors.white,
+                      Color(0xFFFFFF),
+                    ])),
+              ),
+            ],
+          ),
+        ),
+        Positioned(
+          top: 380.h,
+          left: 30.w,
+          right: 30.w,
+          child: Container(
+            width: 315.w,
+            height: 96.h,
+            child: Text(
+              "Best Doctor Appointment App",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Colors_App.main_Blue_Text,
+                  fontSize: 32.dg,
+                  fontWeight: FontWeight.w700),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
---
+++ lib/feature/on_bording/ui/widght/text_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../core/theme/colors.dart';
+
+class Text_Button extends StatelessWidget {
+  const Text_Button({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 315.w,
+      height: 26.h,
+      child: Text(
+        "Manage and schedule all of your medical appointments easily with Docdoc to get a new experience.",
+        textAlign: TextAlign.center,
+        style: TextStyle(
+            color: Colors_App.second_Bleak_Text,
+            fontSize: 10.dg,
+            fontWeight: FontWeight.w400),
+      ),
+    );
+  }
+}
